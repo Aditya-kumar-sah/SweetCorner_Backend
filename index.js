41bccb01ab623ae143e7dda624e4ff38693ab43c
Aditya Kumar Sah <141633697+Aditya-kumar-sah@users.noreply.github.com>
2025-09-26 17:44:39 +0530

    Update index.js
    updated cors for production

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   credentials: true,   // allow cookies
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
